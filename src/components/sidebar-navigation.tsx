f0b2a8d0f7bf0786dfe1937547422e64d7137e46
sowmik-sec <sowmik.sec@gmail.com>
2025-08-18 11:10:55 +0600

    feat: update sidebar navigation with new icons and improved layout for editor modes

--- src/components/sidebar-navigation.tsx
+++ src/components/sidebar-navigation.tsx
@@ -1,6 +1,12 @@
 "use client";
 import React from "react";
-import { Dna, Image as ImageIcon, Type, Wrench, Component } from "lucide-react";
+import {
+  Image as ImageIcon,
+  Type,
+  Wrench,
+  Component,
+  LayoutTemplate,
+} from "lucide-react";
 
 export type EditorMode = "templates" | "elements" | "text" | "tools";
 
@@ -18,51 +24,66 @@ const SidebarNavigation: React.FC<SidebarNavigationProps> = ({
   return (
     <aside className="w-full md:w-20 bg-gray-800 text-white flex md:flex-col items-center p-2 md:py-4 flex-shrink-0">
       <div className="text-2xl font-bold mr-auto md:mr-0 md:mb-8">CG</div>
-      <nav className="flex flex-row md:flex-col space-x-2 md:space-x-0 md:space-y-4">
-        <button
-          onClick={() => setEditorMode("templates")}
-          className={`p-2 rounded-lg transition-colors ${
-            editorMode === "templates" ? "bg-blue-500" : "hover:bg-gray-700"
-          }`}
-          title="Templates"
-        >
-          <Dna size={24} />
-        </button>
+      <nav className="flex flex-row md:flex-col space-x-2 md:space-x-0 md:space-y-6">
+        <div className="flex items-center justify-center flex-col">
+          <button
+            onClick={() => setEditorMode("templates")}
+            className={`p-2 rounded-lg transition-colors ${
+              editorMode === "templates" ? "bg-blue-500" : "hover:bg-gray-700"
+            }`}
+            title="Templates"
+          >
+            <LayoutTemplate size={24} />
+          </button>
+          <span>Design</span>
+        </div>
+        <div className="flex items-center justify-center flex-col">
+          <button
+            onClick={() => setEditorMode("elements")}
+            className={`p-2 rounded-lg transition-colors ${
+              editorMode === "elements" ? "bg-green-500" : "hover:bg-gray-700"
+            }`}
+            title="Elements"
+          >
+            <Component size={24} />
+          </button>
 
-        <button
-          onClick={() => setEditorMode("tools")}
-          className={`p-2 rounded-lg transition-colors ${
-            editorMode === "tools" ? "bg-indigo-500" : "hover:bg-gray-700"
-          }`}
-          title="Tools"
-        >
-          <Wrench size={24} />
-        </button>
-        <button
-          onClick={() => setEditorMode("text")}
-          className={`p-2 rounded-lg transition-colors ${
-            editorMode === "text" ? "bg-yellow-500" : "hover:bg-gray-700"
-          }`}
-          title="Add Text"
-        >
-          <Type size={24} />
-        </button>
-        <button
-          onClick={onImageUpload}
-          className={`p-2 rounded-lg hover:bg-gray-700 transition-colors`}
-          title="Add Image"
-        >
-          <ImageIcon size={24} />
-        </button>
-        <button
-          onClick={() => setEditorMode("elements")}
-          className={`p-2 rounded-lg transition-colors ${
-            editorMode === "elements" ? "bg-green-500" : "hover:bg-gray-700"
-          }`}
-          title="Elements"
-        >
-          <Component size={24} />
-        </button>
+          <span>Elements</span>
+        </div>
+        <div className="flex items-center justify-center flex-col">
+          <button
+            onClick={() => setEditorMode("text")}
+            className={`p-2 rounded-lg transition-colors ${
+              editorMode === "text" ? "bg-yellow-500" : "hover:bg-gray-700"
+            }`}
+            title="Add Text"
+          >
+            <Type size={24} />
+          </button>
+          <span>Text</span>
+        </div>
+        <div className="flex items-center justify-center flex-col">
+          <button
+            onClick={() => setEditorMode("tools")}
+            className={`p-2 rounded-lg transition-colors ${
+              editorMode === "tools" ? "bg-indigo-500" : "hover:bg-gray-700"
+            }`}
+            title="Tools"
+          >
+            <Wrench size={24} />
+          </button>
+          <span>Tools</span>
+        </div>
+        <div className="flex items-center justify-center flex-col">
+          <button
+            onClick={onImageUpload}
+            className={`p-2 rounded-lg hover:bg-gray-700 transition-colors`}
+            title="Add Image"
+          >
+            <ImageIcon size={24} />
+          </button>
+          <span>Uploads</span>
+        </div>
       </nav>
     </aside>
   );
